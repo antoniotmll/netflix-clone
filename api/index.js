7973cb747ef5137ee49028bec1764c4c2010d3b0
antoniotmll <antoniotmll@gmail.com>
2021-09-21 12:49:19 +0200

    Added models

--- api/index.js
+++ api/index.js
@@ -6,9 +6,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("BD Connection successful!"))
+    .catch((err) => console.log(err));
 
 app.listen(8800, () => {
     console.log("Backend server is runnig");
