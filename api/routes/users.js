f6a6077e86451d76aa10f39f326b414ae816b029
antoniotmll <antoniotmll@gmail.com>
2021-09-29 12:49:35 +0200

    Added get all function

--- api/routes/users.js
+++ api/routes/users.js
@@ -53,6 +53,19 @@ router.get("/find/:id", async(req, res) => {
 });
 
 //GET ALL
+router.get("/", verify, async(req, res) => {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+        try {
+            const users = query ? await User.find().limit(10) : await User.find();
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err)
+        }
+    } else {
+        res.status(403).json("You are not allowed to see all users!");
+    }
+});
 
 //GET USER STATS
 
