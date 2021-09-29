46367681cce875108c2d0e0c869291dd85e0ff20
antoniotmll <antoniotmll@gmail.com>
2021-09-29 12:33:03 +0200

    Finished update function

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const app = express();
 const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 
@@ -14,7 +15,9 @@ mongoose.connect(process.env.MONGO_URL, {
     .catch((err) => console.log(err));
 
 app.use(express.json());
+
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen(8800, () => {
     console.log("Backend server is runnig");
